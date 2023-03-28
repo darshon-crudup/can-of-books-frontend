f567de9ec02d03bd9757bbb42f151524e2d0ac57
teamidol <laurencemy@gmail.com>
2023-03-28 15:22:00 -0700

    troubleshooting

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 
 class BestBooks extends React.Component {
@@ -7,7 +8,41 @@ class BestBooks extends React.Component {
       books: []
     }
   }
+ 
+  //   mar28 updates
+getBookss = async () => {
+  try {
+    let url = `${process.env.REACT_APP_SERVER}/books`
 
+    let bookData = await axios.get(url);
+
+    this.setState({
+      books: bookData.data
+    })
+  } catch (error) {
+    console.log(error.response)
+  }
+}
+
+deleteBook = async (id) => {
+  try {
+// TODO: AXIOS is going to send an ID of the book to delete
+    let url = `${process.env.REACT_APP_SERVER}/books/${id}`
+
+    await axios.delete(url);
+
+// TODO: UPDATE STATE TO REMOVE THAT DELETED BOOK
+    let updatedBooks = this.state.books.filter(book => book._id !== id);
+
+    this.setState({
+      books: updatedBooks
+    })
+
+
+  } catch (error) {
+    console.log(error.response)
+  }
+}
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
